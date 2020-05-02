c2386e357cf1f7ddb1f93d4729d654c4e807cc47
yango3 <o3.yang@hotmail.com>
2020-05-02 17:54:04 +0800

    enable autoOpenBrowser

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <div class="input-wrap">
+      <button @click="toLogin">Login</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {}
+  },
+  methods: {
+    toLogin() {
+      // const { $router } = this
+      this.$router.go({ name: 'login' })
+    }
+  }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login">
+    <div class="input-wrap">
+      <input type="text" v-model="name" />
+      <span v-if="error.name" class="err-msg">{{error.name}}</span>
+    </div>
+    <div class="input-wrap">
+      <input type="password" v-model="pwd" />
+      <span v-if="error.pwd" class="err-msg">{{error.pwd}}</span>
+    </div>
+    <div class="input-wrap">
+      <button @click="login">提交</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      name: '',
+      pwd: '',
+      error: {
+        name: '',
+        pwd: ''
+      }
+    }
+  },
+  methods: {
+    check(name, pwd) {
+      if (!name) {
+        this.error.name = '请输入姓名'
+        return false
+      }
+      if (!pwd) {
+        this.error.pwd = '请输入密码'
+        return false
+      }
+      return true
+    },
+    login() {
+      const { $router } = this
+      if (!this.check(this.name, this.pwd)) return
+      if (this.name === 'admin' && this.pwd === '123') {
+        $router.go({ name: 'mainPage' })
+        console.log('go to main page')
+      } else {
+        alert('用户名密码错误')
+      }
+    }
+  }
+}
+</script>
+<style scoped >
+.logn {
+  width: 300px;
+  margin: 100% auto;
+}
+</style>
