2940ca49fabd683d549f7ac973be1a6e61e666a3
yango3 <o3.yang@hotmail.com>
2020-05-04 22:03:53 +0800

    Add vue-resource

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://106.13.104.17:30000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
---
+++ src/api/auth.js
@@ -0,0 +1,14 @@
+const API_URL = '/api/auth'
+
+export default {
+    login(context, name, pwd) {
+        return context.$http({
+            url: API_URL + '/login',
+            method: 'post',
+            data: {
+                name,
+                pwd
+            }
+        })
+    }
+}
