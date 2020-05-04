be7f22a1d6600cee31b3f939a80da95e5d929d95
yango3 <o3.yang@hotmail.com>
2020-05-05 00:32:54 +0800

    Add .prettierrc in root dir

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,14 +1,14 @@
-const API_URL = "/api/auth";
+const API_URL = '/api/auth'
 
 export default {
   login(context, name, pwd) {
     return context.$http({
-      url: API_URL + "/login",
-      method: "post",
+      url: API_URL + '/login',
+      method: 'post',
       data: {
         name,
         pwd
       }
-    });
+    })
   }
-};
+}
